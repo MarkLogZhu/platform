56fec461fde8cea40ae6d65b81e1556a4b6e06f4
MarkLogZhu <markLogZhu@163.com>
2018-05-25 17:20:51 +0800

    搭建SSM框架

---
+++ src/main/java/com/marklogzhu/platform/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.marklogzhu.platform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+
+    @RequestMapping("/print")
+    @ResponseBody
+    public String print(){
+        return "Hello world!";
+    }
+
+}
